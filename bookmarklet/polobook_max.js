9ecda1bb8b7691419e6f3281ed8d8af12d4bcddd
Joseph Farthing <joseph.farthing@gmail.com>
2011-08-29 14:23:26 +0100

    Added licence notice to each file.

--- bookmarklet/polobook_max.js
+++ bookmarklet/polobook_max.js
@@ -1,3 +1,11 @@
+// Copyright 2011 The University of Edinburgh / Joseph Farthing
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
 if (typeof jQuery == 'undefined') {
 	// http://www.hunlock.com/blogs/Howto_Dynamically_Insert_Javascript_And_CSS
 	// modded by Joseph Farthing for UoE
